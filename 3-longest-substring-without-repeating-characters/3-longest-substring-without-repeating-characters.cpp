719b1d786ac3e00705d6e25fe256a2f12fe880c7
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-04 09:36:15 +0530

    Time: 28 ms (48.76%), Space: 9.1 MB (33.12%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> umap;
-        vector<int> ans;
-        int maxlength= 0;
-        
-        int i=0;
-        for(int j=0;j<s.size();j++){
-            if(umap.find(s[j])==umap.end()){
-                umap[s[j]]+=1;
-                maxlength++;
-            }
-            else{
-                ans.push_back(maxlength);
-                while(s[i]!=s[j]){
-                    umap.erase(s[i]);
-                    i++;
-                    maxlength--;
-                }
+int lengthOfLongestSubstring(string s){
+    int maxlength = 0;
+    int uniquechar = 0;
+    unordered_map<char, int> umap;
 
-                i++;
-                maxlength--;
-
-                umap[s[j]] += 1;
-                maxlength++;
-            }
-
+    int i = 0;
+    for (int j = 0; j < s.size();j++){
+        if(umap.find(s[j]) == umap.end()){
+            umap[s[j]]++;
+            uniquechar++;
+        }
+        else{
+            umap[s[j]]++;
         }
 
-        ans.push_back(maxlength);
+        if(uniquechar == j-i+1){
+            maxlength = max(maxlength, j - i + 1);
+        }
+        else if(uniquechar < j-i+1){
+            while(uniquechar < j-i+1){
+                umap[s[i]]--;
+                if(umap[s[i]]==0){
+                    uniquechar--;
+                    umap.erase(s[i]);
+                }
+                i++;
+            }
 
-        return *max_element(ans.begin(),ans.end());
+            if(uniquechar == j-i+1){
+                maxlength = max(maxlength, j - i + 1);
+            }
+        }
     }
+
+    return maxlength;
+}
 };
