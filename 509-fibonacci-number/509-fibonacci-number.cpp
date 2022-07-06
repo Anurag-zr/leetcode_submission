de516a255b91b2bed105f355ad33382d7ab8a742
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-06 08:47:26 +0530

    Time: 4 ms (52.45%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+     if(n==0) return 0;
+     if(n==1) return 1;
+        
+      int prev =1;
+      int secprev =0;
+      int fib; 
+        for(int i=2;i<=n;i++){
+            fib=prev+secprev;
+            secprev=prev;
+            prev=fib;
+        }
+        
+        
+        return fib;
+    }
+};
