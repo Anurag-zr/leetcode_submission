24574b24b174d12a193173dcf2b96f0883727453
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-11 20:07:36 +0530

    Time: 9 ms (75.15%), Space: 13.6 MB (63.14%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    
+    int search(vector<int> &nums,int target,bool findFirst){
+        int n= nums.size();
+        int start=0;
+        int end =n-1;
+        int ans=-1;
+        
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(target<nums[mid]){
+                end = mid -1;
+            }
+            else if(target>nums[mid]){
+                start=mid +1;
+            }
+            else{
+                ans=mid;
+                if(findFirst) end=mid-1;
+                else start = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
         
@@ -7,47 +32,9 @@ public:
             return {-1,-1};
         }
         
-        if(n==1 && nums[0]==target) return {0,0};
-        
-        int firstOccurence = -1;
-        int start =0;
-        int end = n-1;
-        
-        while(start<=end){
-            int mid= start + (end-start)/2;
-            
-            if(nums[mid]==target){
-                firstOccurence = mid;
-                end=mid-1;
-            }
-            else if(target<nums[mid]){
-                end=mid-1;
-            }
-            else{
-                start = mid+1;
-            }
-        }
-        
-        
-        int lastOccurence =-1;
-        start =0;
-        end=n-1;
-        
-        while(start<=end){
-            int mid= start + (end-start)/2;
-            
-            if(nums[mid]==target){
-                lastOccurence = mid;
-                start=mid+1;
-            }
-            else if(target<nums[mid]){
-                end=mid-1;
-            }
-            else{
-                start = mid+1;
-            }
-        }
-        
+        bool findFirst;
+        int firstOccurence = search(nums,target,findFirst=true);
+        int lastOccurence  = search(nums,target,findFirst=false);
         
         return {firstOccurence,lastOccurence};
     }
