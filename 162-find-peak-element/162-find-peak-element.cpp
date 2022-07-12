d215dbdaedb01eb897355a5a330f9175b3360757
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-12 08:43:53 +0530

    Time: 2 ms (91.89%), Space: 8.9 MB (49.70%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -3,47 +3,20 @@ public:
     
     //approach 1
   int findPeakElement(vector<int>& arr) {
-        int n= arr.size();
+        int start =0;
+        int end =arr.size()-1;
         
-        if(n==1){
-            return 0;
-        }
-        
-        int low =0;
-        int high = n-1;
-        
-        while(low<=high){
-            int mid = low +(high-low)/2;
-            
-            if(mid>0 && mid<n-1){
-                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                    return mid;
-                }
-                else if(arr[mid-1]>arr[mid]){
-                    high = mid -1;
-                }
-                else{
-                    low = mid +1;
-                }
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid+1]>arr[mid]){
+                start=mid+1;
             }
-            else if(mid == 0){
-                if(arr[mid]>arr[mid+1]){
-                    return mid;
-                }
-                 
-                return mid+1;
-                
-            }
-            else if(mid == n-1){
-                if(arr[mid]>arr[mid-1]){
-                    return mid;
-                }
-                  
-                   return mid-1;
+            else{
+                end=mid;
             }
         }
         
-        return -1;
+        return start;//or end
     }
         
 };
