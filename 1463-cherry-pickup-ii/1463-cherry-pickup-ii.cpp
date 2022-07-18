04b50314adba37ea7e10c6694e19ef707ba339fd
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-18 09:06:33 +0530

    Time: 133 ms (53.85%), Space: 17 MB (15.39%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     
-    int f(int i,int j1,int j2,vector<vector<int>> &grid, vector<vector<vector<int>>> &dp){
-        int n= grid.size();
-        int m = grid[0].size();
-        
+    int f(int i,int j1,int j2,vector<vector<int>> &grid,int n,int m, vector<vector<vector<int>>> &dp){
+    
         if(j1<0 || j1>=m || j2<0 || j2>=m){
             return -1e8;
         }
@@ -19,8 +17,8 @@ public:
         int maxi=INT_MIN;
         for(int dj1 = -1;dj1<=1;dj1++){
             for(int dj2 = -1; dj2 <=1; dj2++){
-                if(j1==j2) maxi = max(maxi,grid[i][j1]+f(i+1,j1+dj1,j2+dj2,grid,dp));
-                else  maxi = max(maxi,grid[i][j1]+grid[i][j2] + f(i+1,j1+dj1,j2+dj2,grid,dp));
+                if(j1==j2) maxi = max(maxi,grid[i][j1]+f(i+1,j1+dj1,j2+dj2,grid,n,m,dp));
+                else  maxi = max(maxi,grid[i][j1]+grid[i][j2] + f(i+1,j1+dj1,j2+dj2,grid,n,m,dp));
             }
         }
         
@@ -32,6 +30,6 @@ public:
         int m = grid[0].size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int> (m,-1)));
         
-        return f(0,0,m-1,grid,dp);
+        return f(0,0,m-1,grid,n,m,dp);
     }
 };
