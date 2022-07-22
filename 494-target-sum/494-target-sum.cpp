57bdcc4abc2a739b2f5e51d36aef8c76c28fc71a
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-22 10:34:58 +0530

    Time: 11 ms (87.77%), Space: 12.4 MB (38.38%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     
-int f(int ind,int target,vector<int> &arr,map<pair<int,int>,int> &dp){
-    if(ind==-1){
-        if(target==0) return 1;
-        else return 0;
+    int subsetSumTarget(int ind,int target,vector<int> &nums,vector<vector<int>> &dp){
+        
+        if(ind ==0){
+            if(target ==0 && nums[0]==0)  return 2; 
+            else if(target ==0 && nums[0]!=0) return 1;
+            else if(target == nums[0]) return 1;
+            else return 0;
+        }
+        
+        if(dp[ind][target] !=-1) return dp[ind][target];
+        
+        int nottake = subsetSumTarget(ind-1,target,nums,dp);
+        int take = 0;
+        if(nums[ind]<=target) 
+            take = subsetSumTarget(ind-1,target-nums[ind],nums,dp);
+        
+        return dp[ind][target] = take + nottake;
     }
     
-         if(dp.find(make_pair(ind, target)) != dp.end()){
-            return dp[make_pair(ind, target)];
-        }
-
-    
-    int neg = f(ind-1,target+arr[ind],arr,dp);
-    int pos = f(ind-1,target-arr[ind],arr,dp);
-    
-    return dp[make_pair(ind,target)]=neg + pos;
-}
     
     int findTargetSumWays(vector<int>& nums, int target) {
-      int n= nums.size();
-      map<pair<int,int>,int> dp;
-      return f(n-1,target,nums,dp);
+        int n= nums.size();
+        int t_sum =0;
+        
+        for(auto i:nums) t_sum += i;
+        
+      
+        if((t_sum + target)%2==0 && (t_sum+target)>=0) {
+            int n_target = (target+t_sum)/2;
+            vector<vector<int>> dp(n,vector<int> (n_target+1,-1));
+            return subsetSumTarget(n-1,n_target,nums,dp);
+        }
+        else return 0;
     }
 };
