a471f5d0d6516012605cec934c9618003dde44b1
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-07-24 11:34:55 +0530

    Time: 29 ms (63.38%), Space: 12.9 MB (56.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -22,10 +22,10 @@ public:
         // return f(n-1,m-1,text1,text2,dp);
         
         //tabulation
-            vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+            vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
     
-    // for(int i=0;i<=n;i++) dp[i][0]=0;
-    // for(int j=0;j<=m;j++) dp[0][j]=0;
+    for(int i=0;i<=n;i++) dp[i][0]=0;
+    for(int j=0;j<=m;j++) dp[0][j]=0;
     
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
