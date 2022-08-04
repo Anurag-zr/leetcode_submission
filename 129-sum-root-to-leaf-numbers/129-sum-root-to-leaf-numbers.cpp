103eda16b90e7c655cb2fb041e92f968d2c2c289
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-08-04 12:05:24 +0530

    Time: 11 ms (6.69%), Space: 9.7 MB (15.07%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -11,32 +11,29 @@
  */
 class Solution {
 public:
-     void Path(TreeNode* node,vector<int> &ds,vector<int> &ans){
+     void Path(TreeNode* node,int &ds_sum,vector<int> &ans){
      if(node==nullptr) return;
      
      if(node->left==nullptr && node->right == nullptr){ 
-         ds.push_back(node->val);
-         long base =1;
-         int num =0;
-         for(int i = ds.size()-1;i>=0;i--){
-             num+=base*ds[i];
-             base = base*10;
-         }
-        ans.push_back(num);   
-        ds.pop_back();
+        ds_sum= 10*ds_sum +(node->val);
+        ans.push_back(ds_sum);  
+        ds_sum-=node->val;
+        ds_sum/=10;
+    
          return;
      }
      
-     ds.push_back(node->val);
-     Path(node->left,ds,ans);
-     Path(node->right,ds,ans);
-     ds.pop_back();
+     ds_sum= 10*ds_sum +(node->val);
+     Path(node->left,ds_sum,ans);
+     Path(node->right,ds_sum,ans);
+     ds_sum-=node->val;
+     ds_sum/=10;
  }
     
     int sumNumbers(TreeNode* root) {
-      vector<int> ds;
+      int ds_sum=0;
       vector<int> ans;
-        Path(root,ds,ans);
+        Path(root,ds_sum,ans);
         
         int sum=0;
         for(auto i:ans){
