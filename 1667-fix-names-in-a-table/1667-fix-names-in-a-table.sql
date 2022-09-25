d31bcc5a956f3915e848ec6f224aed1b5ec0c8dd
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-09-25 20:32:31 +0530

    Time: 1149 ms (15.75%), Space: 0B (100.00%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# upadate Users
+# set name = (
+          select t.user_id,t.name from(
+            select user_id,concat(ucase(substring(name,1,1)),lcase(substring(name,2))) as name
+            from users
+          )t
+          order by t.user_id;
+# );
