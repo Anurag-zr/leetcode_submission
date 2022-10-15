c7cfec4370c6e07a0a75520bfb3e0535b22caa37
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-10-15 10:05:17 +0530

    Time: 26 ms (85.87%), Space: 13.4 MB (83.37%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    void buildGraph(int n,vector<vector<int>> &prerequisites,vector<vector<int>> &adjList){
+        int m = prerequisites.size();
+        for(int i=0;i<m;i++){
+            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+    }
+    
+    void findIndegree(int n,vector<vector<int>> &adjList,vector<int> &indegree){
+        for(int i=0;i<n;i++){
+            for(auto adjNode : adjList[i]){
+                indegree[adjNode]++;
+            }
+        }
+    }
+    
+    bool detectCycleBfs(int n,vector<vector<int>> &adjList,vector<int> &indegree){
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        int topoSize =0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topoSize++;
+            
+            for(auto adjNode: adjList[node]){
+                indegree[adjNode]--;
+                if(indegree[adjNode]==0) q.push(adjNode);
+            }
+        }
+        
+        if(topoSize == n) return false;
+        return true ;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+       vector<vector<int>> adjList(n,vector<int>());
+        buildGraph(n,prerequisites,adjList);
+        
+        vector<int> indegree(n,0);
+        findIndegree(n,adjList,indegree);
+        
+        return !detectCycleBfs(n,adjList,indegree);
+    }
+};
