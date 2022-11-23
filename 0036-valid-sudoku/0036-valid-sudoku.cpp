422992254148790994f2cf2b55067607daaa3e64
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-11-23 09:56:22 +0530

    Time: 32 ms (80.15%), Space: 18 MB (89.26%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool solve(vector<vector<char>> &board){
+
+ for(int i=0;i<board.size();i++){
+     for (int j = 0; j < board[i].size();j++){
+        if(board[i][j] != '.'){
+                char c= board[i][j];
+                board[i][j]='.';
+                if(!isvalid(board,i,j,c)) return false;
+                board[i][j]=c;
+        }
+     }
+ }
+
+ return true;
+}
+
+bool isvalid(vector<vector<char>> &board,int row,int col,char c){
+    for (int i = 0; i < 9;i++){
+        // if(i == row) continue;
+        // if(i== col) continue;
+        // if((3*(col/3) + i%3) == col) continue;
+        // if((3*(row/3) + i/3) == row) continue;
+        if(board[i][col]== c){
+            return false;
+        }
+
+        if(board[row][i]==c){
+            return false;
+        }
+
+        if(board[3*(row/3) + i/3][3*(col/3) + i%3]==c){
+            return false;
+        }
+
+        
+    }
+    return true;
+}
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        return solve(board);
+    }
+};
