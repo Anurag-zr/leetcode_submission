bea1945831d5b07e6c67e3864af1741bb1947fdf
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2022-12-23 10:30:09 +0530

    Time: 120 ms (52.49%), Space: 31.4 MB (81.11%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans =1;
+        
+        for(auto i:nums){
+            if(i<1) continue;
+            if(i==ans) ans++;
+            if(i>ans) break;
+        }
+        
+        return ans;
+    }
+};
