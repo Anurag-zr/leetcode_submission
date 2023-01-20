7a494fe10e979ed0e7c4abe304664a6d92fb9b4e
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-01-20 20:00:40 +0530

    Time: 33 ms (97.37%), Space: 19.7 MB (95.73%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     
-    void helper(int ind,int prev,vector<int> &ds,set<vector<int>> &ans,vector<int> &nums){
+    void helper(int ind,vector<int> &ds,vector<vector<int>> &ans,vector<int> &nums){
+        if(ds.size()>=2) ans.push_back(ds);
         
-        if(ind == nums.size()){
-            if(ds.size()>=2) ans.insert(ds);
-            return;
+        
+        set<int> hash;
+        for(int i=ind;i<nums.size();i++){
+            if((ds.empty() || nums[i]>=ds.back()) && hash.find(nums[i])==hash.end()){
+                ds.push_back(nums[i]);
+                helper(i+1,ds,ans,nums);
+                ds.pop_back();
+                hash.insert(nums[i]);
+            }
         }
-        
-        //pick
-        if(nums[ind]>=prev){
-            ds.push_back(nums[ind]);
-            helper(ind+1,nums[ind],ds,ans,nums);
-            ds.pop_back();
-        }
-        
-        //not pick
-        
-        helper(ind+1,prev,ds,ans,nums);
     }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-       set<vector<int>> possibleSubseq;
-        vector<int> ds;
-        helper(0,-101,ds,possibleSubseq,nums);
-        
         vector<vector<int>> ans;
         
-        for(auto it: possibleSubseq){
-            ans.push_back(it);
-        }
+        vector<int> ds;
         
-      
+        helper(0,ds,ans,nums);
+        
         return ans;
     }
 };
