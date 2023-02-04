c0e94ce5d1198ca02e2d182764082461fef0b136
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-02-04 10:08:55 +0530

    Time: 21 ms (35.90%), Space: 8.2 MB (22.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -5,19 +5,33 @@ public:
         int m = s2.length();
         
         if(n>m) return false;
+        unordered_map< char,int> map1;
+        unordered_map<char,int> map2;
         
-        sort(s1.begin(),s1.end());
+        for(auto c: s1) map1[c]++;
         
         int i=0;
         for(int j=0;j<s2.length();j++){
             
-            if(j-i+1==n){
-                string temp = s2.substr(i,j-i+1);
-                sort(temp.begin(),temp.end());
+            if(j-i+1<=n){
+                if(map1.find(s2[j])==map1.end()){
+                    map2.clear();
+                    i=j+1;
+                    continue;
+                }
                 
-                if(s1 == temp) return true;
-                else i++;
+                map2[s2[j]]++;
             }
+            
+            if(j-i+1==n){
+                if(map1==map2) return true;
+                else{
+                    map2[s2[i]]--;
+                    if(map2[s2[i]]==0) map2.erase(s2[i]);
+                    i++;
+                }
+            }
+
         }
         
         return false;
