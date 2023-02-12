93149f9f3468f88dfbf56e6fc564dcf7e9aea2ff
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-02-12 12:38:35 +0530

    Time: 887 ms (28.88%), Space: 248.8 MB (8.60%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    vector<long long> dfs(int node,int parent,vector<int> adjList[],int &seats){
+        long long fuelCost,ambassador,availSeats;
+        fuelCost = availSeats =0;
+        ambassador =1;
+        int cars=0;
+        
+        for(auto adjNode: adjList[node]){
+            if(adjNode == parent) continue;
+            
+            vector<long long> temp = dfs(adjNode,node,adjList,seats);
+            fuelCost+=temp[0];
+            ambassador+= temp[1];
+            availSeats+= temp[2];
+        }
+
+        cars = ceil((double)ambassador/seats);
+        availSeats = seats - (ambassador%seats);
+        fuelCost += cars*1;
+        
+        return {fuelCost,ambassador,availSeats};
+    }
+    
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size();
+        vector<int> adjList[n+1];
+        
+        for(auto road: roads){
+            int u = road[0];
+            int v = road[1];
+            
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        
+        long long ans=0;
+        
+        for(auto adjNode : adjList[0]){
+           vector<long long> temp = dfs(adjNode,0,adjList,seats);
+            ans+=temp[0];
+        }
+        
+      return ans;    
+    }
+};
