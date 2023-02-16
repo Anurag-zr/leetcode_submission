140759f5f682828dbfe72702e7d32d932908edc7
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-02-16 12:54:15 +0530

    Time: 75 ms (54.51%), Space: 16.1 MB (64.79%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,27 +1,64 @@
+#include<bits/stdc++.h>
+
 class Solution {
 public:
     
-    int dfs(int ind,vector<string> &arr,string str){
-        if(!isUnique(str)) return 0;
-        
-        int maxi =str.length();
-        
-        for(int i=ind;i<arr.size();i++){
-            maxi = max(maxi,dfs(i+1,arr,str+arr[i]));
-        }
-        
-        return maxi;
+    int maxi=0;
+    
+    static int max(int a,int b){
+        if(a>=b) return a;
+        return b;
     }
     
-    bool isUnique(string s){
-        unordered_set<char> st(s.begin(),s.end());
+    void dfs(int ind,vector<string> &arr,unordered_map<char,bool> &umap){
         
-        if(st.size()==s.length()) return true;
-        return false;
+        for(int i=ind;i<arr.size();i++){
+            
+            if(isUnique(arr[i],umap)){
+                for(auto c: arr[i]) umap[c]=true;
+                // maxi= umap.size();
+                // cout<<arr[i]<<" "<<maxi<<endl;
+                // maxi = max(maxi,dfs(i+1,arr,umap));
+                // cout<<arr[i]<<" "<<maxi<<endl;
+                
+                
+                //-----------//
+                maxi = max(maxi,umap.size());
+                dfs(i+1,arr,umap);
+                for(auto c: arr[i]) umap.erase(c);
+            }
+        }
+        
+        // cout<<maxi<<endl;
+        // return maxi;
+    }
+    
+    bool isUnique(string s, unordered_map<char,bool> &umap){
+        
+        bool allUnique = true;
+        vector<bool> check(26,false);
+        
+        for(int i=0;i<s.length();i++){
+            if(umap.find(s[i])!=umap.end()){
+                allUnique = false;
+                break;
+            }
+            else if(check[s[i]-'a']==true){
+                allUnique = false;
+                break;
+            }
+            else{
+                check[s[i]-'a']=true;   
+            }
+        }
+     return allUnique;
+        
     }
     
     int maxLength(vector<string>& arr) {
-        int maxi = dfs(0,arr,"");
+        unordered_map<char,bool> umap;
+        // int maxi = dfs(0,arr,umap);
+        dfs(0,arr,umap);
         return maxi;
     }
 };
