9fb56e3ec1731b6e2a389cbaaa6ecdefc557d4a4
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-02-22 21:15:16 +0530

    Time: 3 ms (40.02%), Space: 5.8 MB (99.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 2;
-        
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        
-        for(int i=3;i<n+1;i++){
+            if(n<=2) return n;
+    	    int dp[n+1];
             
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        
-        return dp[n];
+            memset(dp,-1,sizeof(dp));
+		    
+		    dp[1]=1;
+		    dp[2]=2;
+		    
+		    for(int i=3;i<=n;i++){
+		        dp[i]= dp[i-1]+dp[i-2];
+		    }
+		    
+		    return dp[n];
     }
 };
