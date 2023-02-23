a070d087e48c6ea4622cf7278ca858fe83d74ed5
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-02-23 13:43:22 +0530

    Time: 228 ms (75.05%), Space: 82.1 MB (53.52%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -5,25 +5,32 @@ public:
         
         int capacity =w;  // capital capicity to perform project;
         
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pqCap;
-        priority_queue<pair<int,int>> pqPro;
+        vector<pair<int,int>> project;
         for(int i=0;i<n;i++){
-            pqCap.push({capital[i],profits[i]});
-            // cout<<pqCap.top().first<<" "<<pqCap.top().second<<endl;
+            project.push_back({capital[i],profits[i]});
         }
+        
+        sort(project.begin(),project.end(),[](pair<int,int> a,pair<int,int> b)->bool{
+            return a.first<b.first;
+        });
+            
+        
+        priority_queue<int> pqPro; //maxHeap
+        // for(int i=0;i<n;i++){
+        //     pqCap.push({capital[i],profits[i]});
+        //     // cout<<pqCap.top().first<<" "<<pqCap.top().second<<endl;
+        // }
+        
+        int i=0;
         
         while(k>0){
 
-            while(!pqCap.empty() && pqCap.top().first<=capacity){
-                int cap = pqCap.top().first;
-                int profit =pqCap.top().second;
-                pqCap.pop();
-                
-                pqPro.push({profit,cap});
+            while(i<n && project[i].first<=capacity){
+                pqPro.push(project[i++].second);
             }
             
             if(!pqPro.empty()){
-                capacity+=pqPro.top().first;
+                capacity+=pqPro.top();
                 pqPro.pop();
                 k--;
             }
