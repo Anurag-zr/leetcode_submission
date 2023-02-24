62cbb4d7c77e053751d3ceb57d5f3aa3bfd6759f
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2023-02-24 18:58:27 +0530

    Time: 329 ms (90.68%), Space: 89.1 MB (26.27%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    int minimumDeviation(vector<int>& nums) {
+//     int minimumDeviation(vector<int>& nums) {
+//         int ans = 1e9;
+//         int mini = 1e9;
+        
+//         priority_queue<int> pq;
+        
+//         for(auto n: nums){
+//             if(n%2) n = n*2;
+//             pq.push(n);
+//             mini = min(mini,n);
+//         }
+        
+//         while(pq.top()%2==0){
+//             ans = min(ans,pq.top()-mini);
+//             mini = min(mini,pq.top()/2);
+//             pq.push(pq.top()/2);
+//             pq.pop();
+//         }
+        
+//         return min(ans,pq.top()-mini);
+//     }
+    
+    //solution 2 using set data structure
+    
+        int minimumDeviation(vector<int>& nums) {
         int ans = 1e9;
         int mini = 1e9;
         
-        priority_queue<int> pq;
+        set<int> st;
         
         for(auto n: nums){
             if(n%2) n = n*2;
-            pq.push(n);
+            st.insert(n);
             mini = min(mini,n);
         }
         
-        while(pq.top()%2==0){
-            ans = min(ans,pq.top()-mini);
-            mini = min(mini,pq.top()/2);
-            pq.push(pq.top()/2);
-            pq.pop();
+        while(*st.rbegin()%2==0){
+            int x = *st.rbegin();
+            st.erase(x);
+            
+            ans = min(ans,x-mini);
+            mini = min(mini,x/2);
+            st.insert(x/2);
         }
         
-        return min(ans,pq.top()-mini);
+        return min(ans,*st.rbegin()-mini);
     }
 };
